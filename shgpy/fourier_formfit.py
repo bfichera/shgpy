3330457501a32a9fca2b03afe6752c9404f16827
Bryan Fichera <bfichera@mit.edu>
2020-05-30 12:19:31 -0500

    Implement logging and Containers into fff.py.

--- shgpy/fourier_formfit.py
+++ shgpy/fourier_formfit.py
@@ -1,7 +1,5 @@
 import sympy as sp
 import numpy as np
-from . import shg_symbols as S
-from . import formula as fx
 from .core import n2i
 from scipy.optimize import basinhopping
 from scipy.optimize import leastsq
@@ -23,6 +21,7 @@ def no_I_component(expr):
 
 
 def leastsq_fit(fform, fdat, guess_dict):
+
     free_symbols = fform.get_free_symbols()
     M = fform.get_M()
 
@@ -39,17 +38,18 @@ def leastsq_fit(fform, fdat, guess_dict):
     return leastsq(residual, guess)
 
 
-def basinhopping_fit(fform_dict, fdata_dict, guess_dict, niter=200, M=16, method='BFGS', verbose=True, stepsize=0.5):
+def basinhopping_fit(fform, fdat, guess_dict, niter, method='BFGS', stepsize=0.5):
     
-    free_symbols = fx.sanitize_free_symbols(fx.extract_free_symbols_from_fform_dict(fform_dict, M=M))
+    free_symbols = fform.get_free_symbols()
+    M = fform.get_M()
 
-    util.oprint(verbose, 'Starting energy function generation.')
-    start = time.perf_counter()
+    logging.debug('Starting energy function generation.')
+    start = time.time()
     energy_expr_list = []
-    for k in sorted(fform_dict.keys()):
+    for k in fform.get_keys():
         for m in np.arange(-M, M+1):
-            expr1 = no_I_component(sp.sympify(fform_dict[k][fx.n2i(m, M)] - fdata_dict[k][fx.n2i(m, M)]))
-            expr2 = I_component(sp.sympify(fform_dict[k][fx.n2i(m, M)] - fdata_dict[k][fx.n2i(m, M)]))
+            expr1 = no_I_component(fform.get_pc(k)[n2i(m, M)]-fdat.get_pc(k)[n2i(m, M)])
+            expr2 = I_component(fform.get_pc(k)[n2i(m, M)]-fdat.get_pc(k)[n2i(m, M)])
             energy_expr_list.append(expr1**2)
             energy_expr_list.append(expr2**2)
 
@@ -57,29 +57,31 @@ def basinhopping_fit(fform_dict, fdata_dict, guess_dict, niter=200, M=16, method
 
     pre_f_energy = sp.lambdify(free_symbols, energy_expr)
     f_energy = lambda x:pre_f_energy(*x)
-    end = time.perf_counter()
-    util.oprint(verbose, 'Done with energy function generation. It took %s seconds.' % (end-start))
+    logging.debug(f'Done with energy function generation. It took {time.time()-start} seconds.')
 
-    x0 = [guess_dict[str(k)] for k in free_symbols]
+    x0 = [guess_dict[k] for k in free_symbols]
     minimizer_kwargs = {'method':method}
-    start = time.perf_counter()
+    logging.debug('Starting basinhopping minimization.')
+    start = time.time()
     ret = basinhopping(f_energy, x0, minimizer_kwargs=minimizer_kwargs, niter=niter, stepsize=stepsize)
-    end = time.perf_counter()
+    ret.time = time.time()-start
+    logging.debug(f'Done with basinhopping minimization. It took {ret.time} seconds.')
 
-    return ret,end-start
+    return ret
 
 
-def basinhopping_fit_with_bounds(fform_dict, fdata_dict, guess_dict, bounds_dict, niter=200, M=16, method='L-BFGS-B', verbose=True, stepsize=0.5):
+def basinhopping_fit_with_bounds(fform, fdat, guess_dict, bounds_dict, niter=200, method='L-BFGS-B', stepsize=0.5):
     
-    free_symbols = fx.sanitize_free_symbols(fx.extract_free_symbols_from_fform_dict(fform_dict, M=M))
+    free_symbols = fform.get_free_symbols()
+    M = fform.get_M()
 
-    util.oprint(verbose, 'Starting energy function generation.')
-    start = time.perf_counter()
+    logging.debug('Starting energy function generation.')
+    start = time.time()
     energy_expr_list = []
-    for k in sorted(fform_dict.keys()):
+    for k in fform.get_keys():
         for m in np.arange(-M, M+1):
-            expr1 = no_I_component(sp.sympify(fform_dict[k][fx.n2i(m, M)] - fdata_dict[k][fx.n2i(m, M)]))
-            expr2 = I_component(sp.sympify(fform_dict[k][fx.n2i(m, M)] - fdata_dict[k][fx.n2i(m, M)]))
+            expr1 = no_I_component(fform.get_pc(k)[n2i(m, M)]-fdat.get_pc(k)[n2i(m, M)])
+            expr2 = I_component(fform.get_pc(k)[n2i(m, M)]-fdat.get_pc(k)[n2i(m, M)])
             energy_expr_list.append(expr1**2)
             energy_expr_list.append(expr2**2)
 
@@ -87,64 +89,55 @@ def basinhopping_fit_with_bounds(fform_dict, fdata_dict, guess_dict, bounds_dict
 
     pre_f_energy = sp.lambdify(free_symbols, energy_expr)
     f_energy = lambda x:pre_f_energy(*x)
-    end = time.perf_counter()
-    util.oprint(verbose, 'Done with energy function generation. It took %s seconds.' % (end-start))
+    logging.debug(f'Done with energy function generation. It took {time.time()-start} seconds.')
 
-    x0 = [guess_dict[str(k)] for k in free_symbols]
+    x0 = [guess_dict[k] for k in free_symbols]
     if bounds_dict is not None:
-        bounds = [bounds_dict[str(k)] for k in free_symbols]
+        bounds = [bounds_dict[k] for k in free_symbols]
     if bounds_dict is None:
         bounds = None
     minimizer_kwargs = {'method':method, 'bounds':bounds}
-    start = time.perf_counter()
+    start = time.time()
+    logging.debug('Starting basinhopping minimization.')
     ret = basinhopping(f_energy, x0, minimizer_kwargs=minimizer_kwargs, niter=niter, stepsize=stepsize)
-    end = time.perf_counter()
+    ret.time = time.time()-start
+    logging.debug(f'Done with basinhopping minimization. It took {ret.time} seconds.')
 
-    return ret,end-start
+    return ret
 
 
-def basinhopping_fit_jac(fform_dict, fdata_dict, guess_dict, niter=200, M=16, method='BFGS', verbose=True, stepsize=0.5):
+def basinhopping_fit_jac(fform, fdat, guess_dict, niter=200, method='BFGS', stepsize=0.5):
     
-    free_symbols = fx.sanitize_free_symbols(fx.extract_free_symbols_from_fform_dict(fform_dict, M=M))
+    free_symbols = fform.get_free_symbols()
+    M = fform.get_M()
 
-    util.oprint(verbose, 'Starting energy function generation.')
-    start = time.perf_counter()
+    logging.debug('Starting energy function generation.')
+    start = time.time()
     energy_expr_list = []
-    for k in sorted(fform_dict.keys()):
+    for k in fform.get_keys():
         for m in np.arange(-M, M+1):
-            expr1 = no_I_component(sp.sympify(fform_dict[k][fx.n2i(m, M)] - fdata_dict[k][fx.n2i(m, M)]))
-            expr2 = I_component(sp.sympify(fform_dict[k][fx.n2i(m, M)] - fdata_dict[k][fx.n2i(m, M)]))
+            expr1 = no_I_component(fform.get_pc(k)[n2i(m, M)]-fdat.get_pc(k)[n2i(m, M)])
+            expr2 = I_component(fform.get_pc(k)[n2i(m, M)]-fdat.get_pc(k)[n2i(m, M)])
             energy_expr_list.append(expr1**2)
             energy_expr_list.append(expr2**2)
 
+    def gradient(expr, free_symbols):
+        return np.array([sp.diff(expr, fs) for fs in free_symbols])
+
     energy_expr = sum(energy_expr_list)
-    denergy_expr = util.gradient(energy_expr, free_symbols)
+    denergy_expr = gradient(energy_expr, free_symbols)
 
     pre_f_energy = sp.lambdify(free_symbols, energy_expr)
     pre_df_energy = sp.lambdify(free_symbols, denergy_expr)
     fdf_energy = lambda x:(pre_f_energy(*x),np.array(pre_df_energy(*x)))
-    end = time.perf_counter()
-    util.oprint(verbose, 'Done with energy function generation. It took %s seconds.' % (end-start))
+    logging.debug(f'Done with energy function generation. It took {time.time()-start} seconds.')
 
-    x0 = [guess_dict[str(k)] for k in free_symbols]
+    x0 = [guess_dict[k] for k in free_symbols]
     minimizer_kwargs = {'method':method, 'jac':True}
-    start = time.perf_counter()
+    logging.debug('Starting basinhopping minimization.')
+    start = time.time()
     ret = basinhopping(fdf_energy, x0, minimizer_kwargs=minimizer_kwargs, niter=niter, stepsize=stepsize)
-    end = time.perf_counter()
+    ret.time = time.time()-start
+    logging.debug(f'Done with basinhopping minimization. It took {ret.time} seconds.')
 
-    return ret, end-start
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return ret
