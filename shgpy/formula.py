cca2f67b4adea6eb306d12cd3c81cf8a74dbca8d
Bryan Fichera <bfichera@mit.edu>
2020-05-29 21:02:13 -0500

    Clean up formula.py after writing fFormContainer

--- shgpy/formula.py
+++ shgpy/formula.py
@@ -1,8 +1,8 @@
 import numpy as np
 import sympy as sp
+from copy import deepcopy
 from . import shg_symbols as S
 from .core import n2i
-from . import tensor as tx
 
 
 def formula_from_fexpr(t, M=16):
@@ -31,21 +31,7 @@ def substitute_into_array(expr_array, *subs_tuples):
     return ans.reshape(expr_array.shape)
 
 
-def extract_free_symbols_from_fform_dict(fform_dict, M=16):
-    free_symbols = tx.union(*[tx.union(*[list(sp.sympify(fform_dict[k][n2i(m, M)]).free_symbols) for m in np.arange(-M, M+1)]) for k in fform_dict.keys()])
-    return free_symbols
-
-
-def sanitize_free_symbols(free_symbols):
-    ans = free_symbols[:]
-    ans.sort(key=lambda x: str(x))
-    return ans
-
-
-def fform_dict_subs(fform_dict, subs_array, M=16):
-    subs_fform_dict = {}
-    for k in fform_dict.keys():
-        subs_fform_dict[k] = np.zeros(shape=(2*M+1,), dtype=object)
-        for m in np.arange(-M, M+1):
-            subs_fform_dict[k][n2i(m)] = sp.sympify(fform_dict[k][n2i(m)]).subs(subs_array)
-    return subs_fform_dict
+def fform_subs(fform, subs_array, M=16):
+    new_fform = deepcopy(fform)
+    new_fform.subs(subs_array)
+    return new_fform
