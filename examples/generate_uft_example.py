bc23230ea49787c1dc4d6010168cf1c18b9788b7
Bryan Fichera <bfichera@mit.edu>
2020-06-01 20:55:50 -0500

    Add example pickle files to .gitignore. Fix bug.

--- examples/generate_uft_example.py
+++ examples/generate_uft_example.py
@@ -4,11 +4,12 @@ import time
 import shgpy
 import shgpy.fformgen
 
-mylogger = logging.getmylogger(__name__)
+mylogger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 AOI = 0.1745
 
 start = time.time()
+mylogger.debug('Starting UFT generation.')
 shgpy.fformgen.generate_uncontracted_fourier_transforms(AOI, 'uft/uft10deg')
 mylogger.debug(f'Finished UFT generation. Took {time.time()-start} seconds.')
