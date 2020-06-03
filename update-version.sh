b46d1c33d81d37f03a0947eba44869a09eaaa1cd
Bryan Fichera <bfichera@mit.edu>
2020-06-03 00:20:05 -0500

    Move update_pypi.sh to update-version.sh

--- update-version.sh
+++ update-version.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+echo $1 > .version
+
 python setup.py sdist bdist_wheel
 python -m twine upload dist/*
+
 rm -r build
 rm -r dist
 rm -r shgpy.egg-info
+
+docsrc/make_docsrc.sh
+docsrc/make_ghpages.sh
