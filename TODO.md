0bb48bde88732165ac56c40d86d25734f013dda9
Bryan Fichera <bfichera@mit.edu>
2020-06-11 12:00:40 -0500

    Add new examples for quadrupole and symbolic cft

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
 # To do
 
 - [ ] Make sure every function can accept a Path instance (for example, everything in ``fformgen.py``)
+- [ ] Document that the tensors are sometimes defined differently in the case of quadrupole compared to dipole.
---
+++ examples/generate_cft_quadrupole_example.py
@@ -0,0 +1,21 @@
+import logging
+import numpy as np
+import sympy as sp
+import time
+
+import shgpy
+import shgpy.fformgen
+import shgpy.tensor_definitions as td
+
+mylogger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+t_dipole = np.zeros(shape=(3,3,3), dtype=sp.Expr)
+
+t_quad = shgpy.particularize(td.quadrupole['D_6h'])
+
+save_filename = 'fform/None-None-D_6h(001)-particularized.p'
+
+start = time.time()
+shgpy.fformgen.generate_contracted_fourier_transforms(save_filename, 'uft/ufttheta', t_dipole, t_quad)
+mylogger.debug(f'Finished CFT generation. Took {time.time()-start} seconds.')
