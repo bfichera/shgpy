65afb06d4f043bdb25c6a0a47a97f3479283b026
Bryan Fichera <bfichera@mit.edu>
2020-07-12 18:42:09 -0400

    Add __version__ variable to __init__.py

--- update-version.sh
+++ update-version.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 echo $1 > .version
+echo -ne "from .core import *\n__version__ = '$1'\n" > shgpy/__init__.py
 
 rm -r build
 rm -r dist
 rm -r shgpy.egg-info
 
 python setup.py sdist bdist_wheel
-python -m twine upload dist/*
+#python -m twine upload dist/*
 
 cd docsrc
 ./make_docsrc.sh
